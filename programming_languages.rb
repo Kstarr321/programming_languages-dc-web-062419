371d929407824295c7a481e9cfa244f39085afbe
Kolton Starr <starrkolton@gmail.com>
2019-05-16 01:45:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,8 @@
 def reformat_languages(hash)
   new_hash = {}
   hash.each do |two, data_hash|
-    
-
     data_hash.each do |lang, val_hash|
       lang.to_sym
-      
-
       val_hash.each do |key, value|
         
         new_hash[lang] = {:type => "#{value}", :style => [two]}
